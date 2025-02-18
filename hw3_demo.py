e759edc9ec185e5d5cbaf07007ea31c971832f68
Sarah Ortiz <sarahortiz@arizona.edu>
2025-02-18 15:35:47 -0700

    Copied first plot to begin to make other linear regressions

--- hw3_demo.py
+++ hw3_demo.py
@@ -13,6 +13,9 @@ def lin_reg(df):
     model = sm.OLS(y, X).fit()
     y_pred = model.predict(X)
 
+    residuals = model.resid  # Get the residuals
+    print("resid: " + residuals)
+
     fig, ax = plt.subplots()
 
     ax.scatter(df["response_time_min"], df["data_compromised"], color="darkorange", alpha=0.5)
@@ -24,6 +27,36 @@ def lin_reg(df):
     ax.set_title("Response Time and Amount of Data Compromised", fontsize=20)
 
     print(model.summary())
+
+# intercept: 257.8809 <-- expected amount of data compromised
+# slope: -0.0117 <--
+
+# 257.8809 + -0.0117*(mins)
+
+
+
+
+def lin_reg2(df):
+    df_dumb = pd.get_dummies(df, columns=["attack_success"], drop_first=True)
+
+    print(df_dumb.attack_success_Yes)
+
+    X = df_dumb["attack_success_Yes"]
+    y = df_dumb["data_compromised"]
+    X = sm.add_constant(X)
+    model = sm.OLS(y, X).fit()
+    y_pred = model.predict(X)
+
+
+    fig, ax = plt.subplots()
+
+    ax.scatter(df_dumb["attack_success_Yes"], df_dumb["data_compromised"], color="darkorange", alpha=0.5)
+
+    ax.plot(df["attack_success_Yes"], y_pred)
+
+    ax.set_xlabel("attack_success_Yes", fontsize=15)
+    ax.set_ylabel("Data Compromised", fontsize=15)
+    ax.set_title("attack_success_Yes and Amount of Data Compromised", fontsize=20)
 
 
 
@@ -46,6 +79,7 @@ def lin_reg(df):
 def main():
     df = pd.read_csv("cybersecurity_incidents.csv")
     lin_reg(df)
+    #lin_reg2(df)
 
     plt.show()
 
